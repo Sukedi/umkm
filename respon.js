3ba40dc1ba19428158c806a9ed954d1a6db94b17
Sukedi <169124018+Sukedi@users.noreply.github.com>
2025-02-19 20:03:24 +0700

    Update respon.js

--- respon.js
+++ respon.js
@@ -41,71 +41,54 @@ function GetResponse(txt){
         rspn2 = "Maaf pesan tidak spesifik. Silakan " + jwb[nn]; 
     }
     // utk pertanyaan² serius...
-    if (ada(["profil"],txt)){ 
-        rspn2 = "Kami adalah UMKM modal mandiri yang bergerak di bidang usaha produk dan jasa. "+
-        "Produk kami berupa minuman herbal dan kuliner. Sedangkan jasa kami berupa developing program dan edukasi komputer. Kami sudah memulai usaha sejak tahun 2021."; }
-    if (ada(["alamat","kantor","lokasi"],txt)){ rspn2 = alamat; }
-    if (ada(["cabang"],txt)){ rspn2 = "Kami tidak buka cabang. "; }
     if (ada(["dagang","awar","layan"],txt)){
-        rspn2 = "Kami melayani <b>Produk</b> dan <b>Jasa</b>. Silakan tulis apa yang ingin anda ketahui. ";
-    }
+        rspn2 = "Kami melayani <b>Produk</b> dan <b>Jasa</b>. Silakan tulis apa yang ingin anda ketahui. ";}
     if (ada(["usaha","bidang"],txt)){
-        rspn2 = "Kami berusaha di bidang <b>Produk</b> dan <b>Jasa</b>. Silakan tulis apa yang ingin anda ketahui. ";
-    }
+        rspn2 = "Kami berusaha di bidang <b>Produk</b> dan <b>Jasa</b>. Silakan tulis apa yang ingin anda ketahui. ";}
     if (ada(["produk"],txt)){ 
-        rspn2 = "Produk kami adalah:"+prdk+sila;
-    }
+        rspn2 = "Produk kami adalah:"+prdk+sila;}
     if (ada(["jasa","mputer"],txt)){ 
-        rspn2 = "Kami melayani jasa:"+jasa+sila;
-    }
+        rspn2 = "Kami melayani jasa:"+jasa+sila;}
     if (ada(["produk"],txt) && ada(["jasa"],txt)){ 
         rspn2 = "Produk kami adalah:"+prdk+
-        "dan jasa kami adalah:"+jasa+sila;
-    }
+        "dan jasa kami adalah:"+jasa+sila;}
     if (ada(["minuman","herbal"],txt)){ 
-        rspn2 = "Produk minuman herbal kami adalah <b>Tjap Duo Kembar</b>: pegal linu, kunyit asam, manjakani, dll. Hub.081392084160. ";
-    }
+        rspn2 = "Produk minuman herbal kami adalah <b>Tjap Duo Kembar</b>: pegal linu, kunyit asam, manjakani, dll. Hub.081392084160. ";}
     if (ada(["kuliner"],txt)){ 
-        rspn2 = "Produk kuliner kami adalah <b>Dapur Bu Dewi</b>: tongseng, rica-rica, dll. Hub.082132431402. ";
-    }
+        rspn2 = "Produk kuliner kami adalah <b>Dapur Bu Dewi</b>: tongseng, rica-rica, dll. Hub.082132431402. ";}
     if (ada(["bakery","roti","kue","bolen"],txt)){ 
-        rspn2 = "Produk bakery/jajanan kami adalah <b>Amar Bolen</b>: roti, bolen, dll. Hub.083825184570. ";
-    }
+        rspn2 = "Produk bakery/jajanan kami adalah <b>Amar Bolen</b>: roti, bolen, dll. Hub.083825184570. ";}
     if (ada(["software"],txt)){ 
         rspn2 = "Software buatan kami bekerja di platform Windows. Kami bangun dengan Microsoft Visual Foxpro. "+
         "Fasilitas software bisa direquest seperti bisa diseting <b>multi-user</b>, ekspor data ke sheet excel, dll.<br>"+
-        "Hubungi:081394641984. ";
-    }
+        "Hubungi:081394641984. ";}
     if (ada(["website"],txt)){ 
         rspn2 = "Program website interaktif buatan kami dibangun dengan menggunakan HTML, CSS, PHP dan JavaScript, "+
         "bekerja sama dengan Internet Service Provider (ISP) sebagai penyedia domain (alamat website) dan hosting (server data). "+
-        "Database yang kami pakai adalah database <b>SQL Server</b>. Hubungi:081394641984. ";
-    }
+        "Database yang kami pakai adalah database <b>SQL Server</b>. Hubungi:081394641984. ";}
     if (ada(["latih","train","rsus","privat","prifat"],txt)){
         rspn2 = "Pelatihan atau kursus IT bisa diikuti oleh siswa, pegawai atau umum dengan pilihan materi: Microsoft Office (Word dan Excel), pembuatan software dan "+
-        "pembuatan website.<br>Hubungi:081394641984. ";
-    }
+        "pembuatan website.<br>Hubungi:081394641984. ";}
     if (ada(["nomor","telp"," wa","whats","kontak"],txt)){
-        rspn2 = "Nomor telpon/whatsapp:<br><ul><li>Jasa komputer: 081394641984</li><li>Produk herbal: 081392084160</li><li>Produk kuliner: 082132431402</li><li>Produk bakery: 083825184570</li></ul>";
-    }
+        rspn2 = "Nomor telpon/whatsapp:<br><ul><li>Jasa komputer: 081394641984</li><li>Produk herbal: 081392084160</li><li>Produk kuliner: 082132431402</li><li>Produk bakery: 083825184570</li></ul>";}
     if (ada(["jam oper","jam kerj","jam berap"],txt)){
-        rspn2 = "Jam kerja kami fleksibel sesuai pesanan pelanggan/klien. Hari minggu kami tidak melayani.";
-    }
+        rspn2 = "Jam kerja kami fleksibel sesuai pesanan pelanggan/klien. Hari minggu kami tidak melayani.";}
     if (ada(["harga","biaya","ongkos"],txt)){
-        rspn2 = "Untuk harga produk atau biaya jasa silakan hubungi langsung nomor telpong/whatsapp kontak masing-masing.";
-    }
+        rspn2 = "Untuk harga produk atau biaya jasa silakan hubungi langsung nomor telpong/whatsapp kontak masing-masing.";}
     if (ada(["deli","order"," do ","pesan","antar"],txt)){
-        rspn2 = "Untuk produk bisa delivery order atau pesan diantar, bisa juga datang langsung ke tempat produksi. Silakan hubungi nomor telpong/whatsapp masing-masing produk.";
-    }
+        rspn2 = "Untuk produk bisa delivery order atau pesan diantar, bisa juga datang langsung ke tempat produksi. Silakan hubungi nomor telpong/whatsapp masing-masing produk.";}
     if (ada(["kemasan"],txt)){
-        rspn2 = "Produk minuman herbal kemasan botol sedangkan produk kuliner kemasan mangkuk sterofom.";
-    }
+        rspn2 = "Produk minuman herbal kemasan botol sedangkan produk kuliner kemasan mangkuk sterofom.";}
     if (ada(["produk"],txt) && ada(["lain"],txt) && !ada(["herbal"],txt) && !ada(["kuliner"],txt)){
-        rspn2 = "Sementara ini kami hanya memproduksi minuman herbal, kuliner dan bakery.";
-    }
+        rspn2 = "Sementara ini kami hanya memproduksi minuman herbal, kuliner dan bakery.";}
     if (ada(["share","serlo","loc"],txt)){
-        rspn2 = "<a href='https://maps.app.goo.gl/ZRk3L6Vtr73NkF2J8?g_st=ac'>Klik di sini untuk buka lokasi di Google Map</a>.";
-    }
+        rspn2 = "<a href='https://maps.app.goo.gl/ZRk3L6Vtr73NkF2J8?g_st=ac'>Klik di sini untuk buka lokasi di Google Map</a>.";}
+    if (ada(["profil"],txt)){ 
+        rspn2 = "Kami adalah UMKM modal mandiri yang bergerak di bidang usaha produk dan jasa. "+
+        "Produk kami berupa minuman herbal, kuliner dan bakery. Sedangkan jasa kami berupa developing program dan edukasi komputer. Kami sudah memulai usaha sejak tahun 2021."; }
+    if (ada(["berdiri","didiri"],txt)){ rspn2 = "Usaha kami berdiri sejak tahun 2021."; }
+    if (ada(["alamat","kantor","lokasi"],txt)){ rspn2 = alamat; }
+    if (ada(["cabang"],txt)){ rspn2 = "Kami tidak buka cabang. "; }
     return rspn1 + "\n" + rspn2;
 }
 function auto_grow(element) {
